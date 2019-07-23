211f0ba7da15ea4dc5d0e3cded8fec931518f6d8
Saul Avila <saul_harry_@hotmail.com>
2019-07-23 15:17:48 -0500

    se agrega configuracion de eureka-client para que los microservicios se registren automaticamente en el servidor de eureka

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,7 +2,14 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/*
+ * Con esta anotación se habilita este proyecto como cliente de eureka; en el
+ * video dice que no es obligatorio hacerlo ya que con el solo hecho de tener la
+ * dependencia se configura pero recomienda hacerlo
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioProductosApplication {
 
