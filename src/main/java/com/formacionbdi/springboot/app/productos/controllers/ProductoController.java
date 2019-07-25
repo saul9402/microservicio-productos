653c6319cc9ca3d41a2d8ffad33aaa5b34caee58
Saul Avila <saul_harry_@hotmail.com>
2019-07-25 10:12:24 -0500

    se agrega CRUD completo para el servicio y el controlador de productos

--- src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -5,8 +5,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
@@ -52,6 +58,27 @@ public class ProductoController {
 		 */
 
 		return producto;
+	}
+
+	@PostMapping(value = "/crear")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Producto crear(@RequestBody Producto producto) {
+		return productoService.save(producto);
+	}
+
+	@PutMapping(value = "/editar/{id}")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoDb = productoService.findById(id);
+		productoDb.setNombre(producto.getNombre());
+		productoDb.setPrecio(producto.getPrecio());
+		return productoService.save(productoDb);
+	}
+
+	@DeleteMapping(value = "/eliminar/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		productoService.deleteById(id);
 	}
 
 }
--- src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/service/IProductoService.java
@@ -10,5 +10,9 @@ public interface IProductoService {
 	public List<Producto> findAll();
 
 	public Producto findById(Long id);
+	
+	public Producto save(Producto producto);
+	
+	public void deleteById(Long id);
 
 }
