436a374804e7fdd9de03c3e287efeb5d0919753b
Saul Avila <saul_harry_@hotmail.com>
2019-07-25 12:18:08 -0500

    se agrega biblioteca commons para evitar repetir clases en los microservicios y centralizar en un solo lugar

--- src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,15 +2,22 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/*
+/**
  * Con esta anotación se habilita este proyecto como cliente de eureka; en el
  * video dice que no es obligatorio hacerlo ya que con el solo hecho de tener la
  * dependencia se configura pero recomienda hacerlo
  */
 @EnableEurekaClient
 @SpringBootApplication
+/**
+ * Le indicas donde busar la entidades, ya que de no hacerlo no las va a
+ * encontrar puesto que no pertenece al package base.
+ * 
+ */
+@EntityScan({ "com.formacionbdi.springboot.app.commons.models.entity" })
 public class SpringbootServicioProductosApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/formacionbdi/springboot/app/productos/models/dao/ProductoDao.java
+++ src/main/java/com/formacionbdi/springboot/app/productos/models/dao/ProductoDao.java
@@ -3,7 +3,7 @@ package com.formacionbdi.springboot.app.productos.models.dao;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.formacionbdi.springboot.app.productos.models.entity.Producto;
+import com.formacionbdi.springboot.app.commons.models.entity.Producto;
 
 @Repository
 public interface ProductoDao extends CrudRepository<Producto, Long> {
